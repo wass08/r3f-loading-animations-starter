311d74ab0afd733818ff4c4facda251dd64ef0de
wass08 <wass08@gmail.com>
2023-03-01 16:38:05 +0900

    remove fog default

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <Canvas shadows camera={{ position: [-5, 1, 6], fov: 25 }}>
-        <fog attach="fog" args={["#16a04b", 12, 30]} />
         <Experience />
       </Canvas>
     </>
